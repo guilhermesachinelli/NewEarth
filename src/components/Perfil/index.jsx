801ba0ae468e1f8efb073b1b5de2599c1d30a66d
GuiLimaSeila <guilhermejeronimolima@gmail.com>
2024-04-29 08:34:53 -0300

    feat: Creat Perfil

---
+++ src/components/Perfil/index.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+import styles from "./styles";
+
+const Perfil = ({ perfil }) => {
+  return (
+    <View>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+};
+
+export default Title;
---
+++ src/screens/Perfils/index.jsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import styles from "./styles";
+import Perfil from "../../components/Perfil";
+
+export default function Perfils() {
+
+  return (
+    <View style={styles.container}>
+    <Perfil/>
+    </View>
+  );
+}
