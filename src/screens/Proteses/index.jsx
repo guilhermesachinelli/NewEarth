98edbcbec3c246eec2ad8b041f3c4f0858698571
GuiLimaSeila <guilhermejeronimolima@gmail.com>
2024-05-06 11:48:34 -0300

    feat: Creat Perfil

--- src/screens/Proteses/index.jsx
+++ src/screens/Proteses/index.jsx
@@ -6,9 +6,9 @@ export default function Proteses() {
     return (
         <ScrollView contentContainerStyle={styles.container}>
             <Image
-                    source={require('../../../assets/mulherBionica.jpg')}
-                    style={styles.imageHeader}
-                />
+                source={require('../../../assets/mulherBionica.jpg')}
+                style={styles.imageHeader}
+            />
             <Text style={styles.textTitle1}>Bem vindos onde a inspiração encontra a realidade.</Text>
             <View style={styles.containerzinho} >
                 <Image
@@ -17,14 +17,14 @@ export default function Proteses() {
                 />
                 <Text style={styles.textTitle}>Rheo Knee</Text>
                 <Text style={styles.info} >
-A Rheo Knee é uma prótese de joelho inovadora que combina tecnologia avançada com sustentabilidade. Com sensores e algoritmos inteligentes, proporciona uma experiência de mobilidade natural e independente, permitindo atividades como subir escadas e correr. Projetada com materiais duráveis e recicláveis, reduzindo seu impacto ambiental. Restaura não apenas a mobilidade, mas também a independência e autoconfiança, transformando vidas e promovendo um futuro mais inclusivo e sustentável.</Text>
+                    A Rheo Knee é uma prótese de joelho inovadora que combina tecnologia avançada com sustentabilidade. Com sensores e algoritmos inteligentes, proporciona uma experiência de mobilidade natural e independente, permitindo atividades como subir escadas e correr. Projetada com materiais duráveis e recicláveis, reduzindo seu impacto ambiental. Restaura não apenas a mobilidade, mas também a independência e autoconfiança, transformando vidas e promovendo um futuro mais inclusivo e sustentável.</Text>
             </View>
 
 
             <View style={styles.containerzinho}>
                 <Image
                     source={require('../../../assets/geniumX3.png')}
-                    style={styles.image}/>
+                    style={styles.image} />
                 <Text style={styles.textTitle}>Genium X3</Text>
                 <Text style={styles.info} >O Genium X3, redefine os padrões de mobilidade para amputados de membros inferiores. Com sensores e algoritmos sofisticados, proporciona uma marcha natural e adaptativa, enfrentando desafios do mundo real com precisão. Sua durabilidade e resistência à água promovem uma vida ativa e independente, capacitando os usuários a superar obstáculos e desfrutar de uma melhor qualidade de vida.</Text>
             </View>
@@ -33,10 +33,10 @@ A Rheo Knee é uma prótese de joelho inovadora que combina tecnologia avançada
             <View style={styles.containerzinho}>
                 <Image
                     source={require('../../../assets/foot.png')}
-                    style={styles.image}/>
+                    style={styles.image} />
                 <Text style={styles.textTitle}>Proprio Foot</Text>
                 <Text style={styles.info} >
-O Proprio Foot é uma prótese de pé inovadora que combina tecnologia avançada e design ergonômico para proporcionar uma experiência de caminhada natural e confortável. Com sensores e algoritmos inteligentes, adapta-se dinamicamente ao usuário, oferecendo uma marcha suave e equilibrada em diversos terrenos. Projetado para reduzir fadiga e desconforto, restaura a funcionalidade e estabilidade do pé, transformando vidas e promovendo mobilidade eficiente.</Text>
+                    O Proprio Foot é uma prótese de pé inovadora que combina tecnologia avançada e design ergonômico para proporcionar uma experiência de caminhada natural e confortável. Com sensores e algoritmos inteligentes, adapta-se dinamicamente ao usuário, oferecendo uma marcha suave e equilibrada em diversos terrenos. Projetado para reduzir fadiga e desconforto, restaura a funcionalidade e estabilidade do pé, transformando vidas e promovendo mobilidade eficiente.</Text>
             </View>
 
 
@@ -47,7 +47,7 @@ O Proprio Foot é uma prótese de pé inovadora que combina tecnologia avançada
                 />
                 <Text style={styles.textTitle}>T63</Text>
                 <Text style={styles.info} >
-A prótese T63 é uma solução avançada para atletas de corrida amputados, oferecendo desempenho máximo e resposta rápida. Leve e aerodinâmica, proporciona estabilidade e eficiência para alcançar altas velocidades. Seu design permite uma transição suave entre passadas e uma sensação de propulsão eficiente. Ajustável e personalizável, garante conforto e eficácia durante as competições. Popular entre corredores de elite, proporciona uma experiência de corrida semelhante à de atletas não amputados</Text>
+                    A prótese T63 é uma solução avançada para atletas de corrida amputados, oferecendo desempenho máximo e resposta rápida. Leve e aerodinâmica, proporciona estabilidade e eficiência para alcançar altas velocidades. Seu design permite uma transição suave entre passadas e uma sensação de propulsão eficiente. Ajustável e personalizável, garante conforto e eficácia durante as competições. Popular entre corredores de elite, proporciona uma experiência de corrida semelhante à de atletas não amputados</Text>
             </View>
 
 
@@ -70,7 +70,7 @@ A prótese T63 é uma solução avançada para atletas de corrida amputados, ofe
                 <Text style={styles.info} >A cadeira de rodas para atletismo na classe T51 é essencial para atletas paralímpicos que competem em corridas de pista e campo. Projetada para leveza, resistência e aerodinâmica, proporciona uma condução suave e eficiente, permitindo velocidades impressionantes. Altamente ajustável e personalizável, adapta-se às necessidades individuais de cada atleta. Combina tecnologia avançada com engenharia de precisão, capacitando os atletas a alcançarem seus objetivos e superarem limites no esporte paralímpico.</Text>
             </View>
 
-<FooterNew />
+            <FooterNew />
         </ScrollView>
     );
 }
